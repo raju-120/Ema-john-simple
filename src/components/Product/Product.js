5d92db3f4488b5c3296736342eb389dec7c2a05a
Raju <ashfaqraju27@gmail.com>
2022-09-29 14:50:28 +0600

    done

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -3,21 +3,24 @@ import {  faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
 import './Product.css'
 
-const Product = (props) => {
-    const {name, img, seller, price, ratings} = props.product;
+const Product = ({product, handleAddToCart}) => {
+    const {name, img, seller, price, ratings} = product;
     
     
     return (
         <div className='product'>
             <img src={img} alt="" />
             <div className='product-info'>
+
                 <p className='product-name'>{name}</p>
                 <p>Price: ${price}</p>
                 <p> <small>Seller: {seller}</small> </p>
                 <p> <small>Ratings: {ratings} stars</small></p>
             </div>
-            <button onClick={ () => props.handleAddToCart(props.product)} className='button-cart'>
+            
+            <button onClick={ () => handleAddToCart(product)} className='button-cart'>
                 <p className='btn-text'>Add To Cart</p>
+
                 <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
             </button>
         </div>
